99d6c343f3f8357fd5d93e6e9431463143a9d330
mayank gupta <mayankgupta.edu@gmail.com>
2023-10-06 02:39:35 +0530

    db connection

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to MongoDb");
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 app.listen(3000,()=>{
     console.log("server Listening on port 3000");
