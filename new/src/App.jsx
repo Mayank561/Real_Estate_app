88385c74c7136540dbf636c380405824410a0526
mayank gupta <mayankgupta.edu@gmail.com>
2023-10-06 01:53:58 +0530

    working

---
+++ new/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+    </Routes>
+        
+      
+    </BrowserRouter>
+  )
+}
+
+export default App;
