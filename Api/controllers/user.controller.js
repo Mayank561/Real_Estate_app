6e6d9c405e1c34b7119e4a98440001c15e335555
mayank gupta <mayankgupta.edu@gmail.com>
2023-10-12 23:58:00 +0530

    pending

---
+++ Api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const test = (req,res)=>{
+    res.json({
+        message: "hello from server side",
+    });
+}
+
+
+module.exports = test;
---
+++ Api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const userRouter = require("./routes/user.route");
+const authRoute = require("./routes/auth.route");
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to MongoDb");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+app.use(express.json());
+
+app.listen(3000,()=>{
+    console.log("server Listening on port 3000");
+});
+
+// define routes
+app.use("/api/user", userRouter);
+app.use("/api/auth",authRoute);
