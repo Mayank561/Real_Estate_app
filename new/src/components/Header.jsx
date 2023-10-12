493b1dc8d02bc5ea2cbdbdaede38d69a50a9cf04
mayank gupta <mayankgupta.edu@gmail.com>
2023-10-13 00:20:39 +0530

    pending

--- new/src/components/Header.jsx
+++ new/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 // import {FaSearch} from "react-icons/fa";
 import {Link} from 'react-router-dom';
 function Header() {
@@ -34,32 +33,3 @@ function Header() {
 }
 
 export default Header
-=======
-import {Link} from "react-router-dom";
-function Header() {
-  return (
-    <div
-      className="bg-slate-200">
-      <div className='flex justify-between items-center
-      max-w-6xl mx-auto p-3'>
-        <Link to="/">
-        <h1 className='font-bold'>Auth App</h1>
-        </Link>
-        <ul className='flex gap-4'>
-            <Link>
-            <li>Home</li>
-            </Link>
-            <Link to ="/about">
-            <li>About</li>
-            </Link>
-            <Link to ="/about">
-            <li>Sign In</li>
-            </Link>
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-export default Header
->>>>>>> 99d6c343f3f8357fd5d93e6e9431463143a9d330
